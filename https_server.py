0d8d06aea2dce60495555a61718819cf02e3ef6c
Adeen Shukla <adeens357@gmail.com>
2018-03-23 15:43:23 +0530

    Script to start https local server

---
+++ https_server.py
@@ -0,0 +1,7 @@
+import http.server, ssl
+
+server_address = ('localhost', 8000)
+httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket, server_side = True, certfile = '../../server.pem', ssl_version = ssl.PROTOCOL_TLSv1)
+
+httpd.serve_forever()
