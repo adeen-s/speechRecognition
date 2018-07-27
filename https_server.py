96140cb01072c85632572bd0f3f3d29bba3c2054
Adeen Shukla <adeen@adeen.me>
2018-07-27 16:17:34 +0530

    Add server config to same directory

--- https_server.py
+++ https_server.py
@@ -2,6 +2,6 @@ import http.server, ssl
 
 server_address = ('localhost', 8000)
 httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket, server_side = True, certfile = '../../server.pem', ssl_version = ssl.PROTOCOL_TLSv1)
+httpd.socket = ssl.wrap_socket(httpd.socket, server_side = True, certfile = 'server/server.pem', ssl_version = ssl.PROTOCOL_TLSv1)
 
 httpd.serve_forever()
